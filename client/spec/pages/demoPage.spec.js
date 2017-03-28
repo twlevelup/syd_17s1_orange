f67d828822460c65797a7350f02a22571db12699
Vishnu Ganesan <vishnukg@gmail.com>
2017-03-28 17:06:15 +1100

    Removed lint errors and improved test speed

--- client/spec/pages/demoPage.spec.js
+++ client/spec/pages/demoPage.spec.js
@@ -3,20 +3,20 @@ const DemoPage = require('../../src/js/pages/demoPage');
 let page;
 
 describe('The Demo Page', () => {
-    beforeEach(() => {
-      page = new DemoPage();
+  beforeEach(() => {
+    page = new DemoPage();
+  });
+
+  describe('rendering', () => {
+    it('should produce the correct HTML', () => {
+      page.render();
+      expect(page.$el).toContainText('This is a demo');
+      expect(page.$el).toContainHtml('<p>What a great page!</p>');
     });
 
-    describe('rendering', () => {
-        it('should produce the correct HTML', () => {
-          page.render();
-          expect(page.$el).toContainText('This is a demo');
-          expect(page.$el).toContainHtml('<p>What a great page!</p>');
-        });
-
-        it('should pass a variable to the template', () => {
-          page.render();
-          expect(page.$el).toContainHtml('<h2>Welcome, John Smith!</h2>');
-        });
+    it('should pass a variable to the template', () => {
+      page.render();
+      expect(page.$el).toContainHtml('<h2>Welcome, John Smith!</h2>');
     });
+  });
 });
