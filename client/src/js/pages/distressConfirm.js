0300097ec8fac7b6c9dcd44e710583377459e0ac
Michelle Leman <michelleleman@Michelles-MacBook-Air-3.local>
2017-04-18 21:15:47 +1000

    [Michelle Wing][Added phone no and gps to distress signal]

--- client/src/js/pages/distressConfirm.js
+++ client/src/js/pages/distressConfirm.js
@@ -1,9 +1,13 @@
 const Page = require('watch_framework').Page;
 const $ = require('jquery');
 
+
+const storage = require('../../storage');
 const template = require('../../templates/pages/distressConfirm.hbs');
 
 const distressConfirm = Page.extend({
+  // var longitude;
+  // var latitude;
   id: 'distressConfirm',
   template,
 
@@ -23,8 +27,16 @@ const distressConfirm = Page.extend({
     window.App.navigate('distress');
   },
 
+  // get location info from browser
+  // navigator.geolocation.getCurrentPosition(function(location) {
+  //   // latitude = console.log(location.coords.latitude);
+  //   // longitude = console.log(location.coords.longitude);
+  //   // console.log(location.coords.accuracy);
+  //   this.$el.html(this.template({ phone: storage.myPhone, lat: location.coords.latitude, long: location.coords.longitude}));
+  // });
+
   render() {
-    this.$el.html(this.template());
+    this.$el.html(this.template({ phone: storage.myPhone, lat: storage.myLat, long: storage.myLong}));
     $('#watch').addClass('case distressconfirm');
     return this;
   },
